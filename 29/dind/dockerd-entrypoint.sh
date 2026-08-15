00dff1df7f1097f4b2229ce9c1b9a8e712fe224c
Sebastiaan van Stijn <github@gone.nl>
2026-08-15 17:36:34 +0200

    remove containerd, ctr and use embedded containerd
    before/after:
    
        docker image ls dind
    
        IMAGE         ID             DISK USAGE   CONTENT SIZE   EXTRA
        dind:before   6f84404d1f9d        528MB          137MB
        dind:latest   5ac80767b941        436MB          112MB
    
    Signed-off-by: Sebastiaan van Stijn <github@gone.nl>

--- 29/dind/dockerd-entrypoint.sh
+++ 29/dind/dockerd-entrypoint.sh
@@ -124,6 +124,7 @@ if [ "$#" -eq 0 ] || [ "${1#-}" != "$1" ]; then
 		set -- dockerd \
 			--host="$dockerSocket" \
 			--host=tcp://0.0.0.0:2376 \
+			--feature "embedded-containerd" \
 			--tlsverify \
 			--tlscacert "$DOCKER_TLS_CERTDIR/server/ca.pem" \
 			--tlscert "$DOCKER_TLS_CERTDIR/server/cert.pem" \
@@ -135,6 +136,7 @@ if [ "$#" -eq 0 ] || [ "${1#-}" != "$1" ]; then
 		set -- dockerd \
 			--host="$dockerSocket" \
 			--host=tcp://0.0.0.0:2375 \
+			--feature "embedded-containerd" \
 			"$@"
 		DOCKERD_ROOTLESS_ROOTLESSKIT_FLAGS="${DOCKERD_ROOTLESS_ROOTLESSKIT_FLAGS:-} -p 0.0.0.0:2375:2375/tcp"
 	fi
